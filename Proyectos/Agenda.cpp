2a54a0245956ce07ea28401244d4953ba15ecc56
RicardoPayan0117 <112823481+RicardoPayan0117@users.noreply.github.com>
2023-09-06 20:08:57 -0700

    Agenda 4 y examen 1

--- Proyectos/Agenda.cpp
+++ Proyectos/Agenda.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string.h>
+#include <iomanip>
+#include <ctime>
 
 using namespace std;
 
 	int opcion;		//Lo usa el switch para elegir las opciones.
 	int entrar = 1, cimaEventos = 0; // entrar mantiene el programa abierto, cimaEventos marca un espacio libre para agendar un evento.
 	string eventos[100][5];    //Esta matris guarda los datos de los eventos(fecha, hora, lugar, nombre y descripcion).
-	char dato[100];
+	string dato;
 	
 void menu(){ //Muestra las opciones del programa
 	system("color 0d");
@@ -43,20 +45,29 @@ void salir(){ //void opcion 0.{
 void ingresarEvento(){ //void opcion 1.
 	system("color 0a");
 	
-	cout << "Ingrese el nombre del evento: " <<endl;
-	cin  >> eventos[cimaEventos][1];
-	//cin.getline(dato,100,'\n');	//cin  >> Eventos[cimaEvento][1];//ingresa el dato del evento en la cima(espacio libre mas alto de la matriz)
-	//	eventos[cimaEventos][1] = dato;
-	cout << "Describe el evento: " <<endl;
-	cin  >> eventos[cimaEventos][2];
-	cout << "Ingrese el lugar del evento: " <<endl;
-	cin  >> eventos[cimaEventos][3];
-	cout << "Ingrese la fecha del evento: " <<endl;
-	cin  >> eventos[cimaEventos][4];
-	cout << "Ingrese la hora del evento: " <<endl;
-	cin  >> eventos[cimaEventos][5];
-	cimaEventos++;//eleva el valor de cima en uno para apuntar al siguiente espacio libre.
+	cout << "Ingrese el nombre del evento: ";
+	cin.ignore();                       //No se para que es pero si lo quito no jala =(
+	getline(cin, dato);                 //Pide el nombre del evento-
+		eventos[cimaEventos][1] = dato; //Almacena el evento en la matriz para guardarlo.
+		
+	cout << "Describe el evento: ";
+	getline(cin, dato); 
+		eventos[cimaEventos][2] = dato;
+		
+	cout << "Ingrese el lugar del evento: ";
+	getline(cin, dato); 
+		eventos[cimaEventos][3] = dato;
+		
+	cout << "Ingrese la fecha del evento: ";
+	getline(cin, dato); 
+		eventos[cimaEventos][4] = dato;
+		
+	cout << "Ingrese la hora del evento: ";
+	getline(cin, dato); 
+		eventos[cimaEventos][5] = dato;
 	
+	cimaEventos++;//eleva el valor de cima en uno para apuntar al siguiente espacio libre.
+
 } //Fin ingresar evento.
 
 void mostrarEventos() //void opcion 5.
@@ -65,12 +76,34 @@ void mostrarEventos() //void opcion 5.
  	
  	if (cimaEventos == 0){ // revisa que haya tareas para mostar.
 	cout<< "No hay eventos para mostar." <<endl;
-	}else{//si hay tareas busca.
-		for(int i = 0; i < cimaEventos; i++){ 
-		cout << "Evento #" << i << " " << eventos[i][1] << " " << eventos[i][2]  << " " << eventos[i][3]  << " " << eventos[i][4]  << " " << eventos[i][5] <<endl;  
+	}else{//si hay tareas imprime.
+		for(int i = 0; i < cimaEventos; i++){ //empieza desde el primer fila de la matriz hasta la cima que seria la ultima fila con eventos.  
+		cout << "| Evento #" << i << " | Nombre: " << eventos[i][1] << " | Descripcion: " << eventos[i][2]  << " |Lugar: " << eventos[i][3]  << " | Fecha: " << eventos[i][4]  << " | Hora: " << eventos[i][5] << " |"<<endl;  
 		}
  	}
  }
+
+void editarEvento(){
+	cout << "Ingrese el numero de evento: " <<endl;
+	cin  >> opcion;
+	
+		if (opcion == false){ //este if compruba que el usuario este ingresando un numero y no una letra/caracter especial.
+		cout << "Debe ingresar el numero de evento." <<endl;
+		cin.clear();
+		fflush(stdin);
+		editarEvento();
+		
+	}else{
+		if(opcion > 0 and opcion < cimaEventos){ // Este if comprube que el numero que ingreso el usuario exista en la matriz.
+				cout << "El evento a editar es el siguiente: "<<endl;	
+	cout << "| Evento #" << opcion << " | Nombre: " << eventos[opcion][1] << " | Descripcion: " << eventos[opcion][2]  << " |Lugar: " << eventos[opcion][3]  << " | Fecha: " << eventos[opcion][4]  << " | Hora: " << eventos[opcion][5] << " |"<<endl;  
+		
+		}else{
+				cout << "El numero que ingreso no existe en la matriz. "<<endl;
+				editarEvento();
+			 } //Fin comprobacion numero valido.
+		 } //Fin comprobacion numero.
+} //Fin void editarEvento;
 
 int main(){
 	system("color 0d");
@@ -94,8 +127,12 @@ int main(){
         		return 0;
         	break; //Fin case 9;
         	
-            case 1: // Opcion 1(Ingresar una tarea). 
+            case 1: // Opcion 1(Permite ingresar una tarea nueva). 
             	ingresarEvento();               
+            break;
+            
+            case 2: // Opcion 2(Permite editar una tarea ya existente). 
+            	editarEvento();               
             break;
             
              case 4: // Opcion 5(Mostrar todos los eventos). 
---
+++ capitulo 1/Examen 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int numero;
+
+int main(){
+	
+	cout << "Ingrese un numero entero: ";
+	cin >> numero;
+	
+	cout <<endl;
+	cout << "El doble del numero entero es : " << numero * 2 <<endl;
+}
